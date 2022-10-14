c6c3043a9ffda3d011938d836e9e00e170b21e8e
olivergav <oliwier.gawlik0@gmail.com>
2022-10-14 14:56:57 +0200

    auth provider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+    const [auth, setAuth] = useLocalStorage("accessToken", {});
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
