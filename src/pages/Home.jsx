fe9b32955bc20ba202b231b370eb540ab73f9dc3
olivergav <oliwier.gawlik0@gmail.com>
2022-10-14 17:10:12 +0200

    add infographics to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import LeadText from "../components/leadText/LeadText";
 import Button from "../components/button/Button";
+import Infographics from "../components/infographics/Infographics";
 
 function Home(){
     return (
         <main className='box center'>
             <LeadText/>
             <Button kind='primary' text='Logowanie' cb={() => {}}/>
+            <Infographics/>
         </main>
     )
 }
