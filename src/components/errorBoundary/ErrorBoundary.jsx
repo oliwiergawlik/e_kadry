ba55404ef917b942e5a22fa5150b3e8c0d927538
olivergav <oliwier.gawlik0@gmail.com>
2022-10-14 15:07:26 +0200

    add error boundary

---
+++ src/components/errorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,44 @@
+import {Component} from "react";
+import {Link, Navigate} from "react-router-dom";
+
+class ErrorBoundary extends Component {
+    state = {
+        hasError: false,
+        redirect: false
+    }
+
+    static getDerivedStateFromError() {
+        return {
+            hasError: true
+        };
+    }
+
+
+    componentDidCatch(error, errorInfo) {
+        console.log('Error boundary caught an error', error, errorInfo);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.state.hasError) {
+            setTimeout(() => {
+                this.setState({redirect: true})
+            }, 5000)
+        }
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (
+                <Navigate to="/"/>
+            )
+        } else if (this.state.hasError) {
+            return (
+                <h2>There was an error! <Link to="/">Click here.</Link></h2>
+            )
+        }
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundary;
--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {AuthProvider} from "./context/AuthProvider";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/*" element={<App />} />
-                </Routes>
-            </AuthProvider>
-        </BrowserRouter>
+        <ErrorBoundary>
+            <BrowserRouter>
+                <AuthProvider>
+                    <Routes>
+                        <Route path="/*" element={<App/>}/>
+                    </Routes>
+                </AuthProvider>
+            </BrowserRouter>
+        </ErrorBoundary>
     </React.StrictMode>
 );
 
